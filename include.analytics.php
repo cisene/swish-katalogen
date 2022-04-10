4004f7b2243df01620e7cc1f7fe83546f7b4d83c
Christopher Isene <christopher.isene@gmail.com>
2022-04-10 16:24:02 +0000

    Updated analytics code

--- include.analytics.php
+++ include.analytics.php
@@ -1,1 +1,3 @@
       <!-- Global site tag (gtag.js) - Google Analytics --><script async src="https://www.googletagmanager.com/gtag/js?id=UA-728601-16"></script><script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'UA-728601-16');</script>
+      <!-- Global site tag (gtag.js) - Google Analytics --><script async src="https://www.googletagmanager.com/gtag/js?id=G-P0B00FWD7S"></script><script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-P0B00FWD7S');</script>
+
