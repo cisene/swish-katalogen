f1de020a3a5038a3c0ec79065988c113023d380f
Christopher Isene <christopher.isene@gmail.com>
2023-08-27 14:43:35 +0200

    Removing QR code from footer

--- include.pagefooter.php
+++ include.pagefooter.php
@@ -7,10 +7,10 @@
         <h3>Swish-Katalogen</h3>
         <p>Alla samlade Swish-nummer kommer från projektet <a href="https://github.com/cisene/swish-123" target="_blank">Swish-123</a> på Github. Alla nummer har samlats av individer eftersom Swish själva inte tillhandahåller möjligheten att söka eller ta reda på vem/vilka dessa nummer tillhör. Läs mer eller bidra här: <a href="https://github.com/cisene/swish-123" target="_blank">Swish-123</a>.</p>
         <p>Om du gillar tjänsten eller bara vill skänka mig en slant som tack för hjälpen, Swisha mig på nummer 070<span></span>3852<span></span>166 - Ingen peng är för liten.</p>
-        <div>
+        <!-- div>
           <svg width="400" height="400">
             <image xlink:href="/swish-katalogen/qr/0703852166" width="400" height="400" title="Swishnummer 0703852166"/>
           </svg>
-        </div>
+        </div -->
       </div>
 
