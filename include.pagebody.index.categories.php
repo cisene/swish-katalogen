28d3c99db5d32370431266d55b5457a7e84671e3
Christopher Isene <christopher.isene@gmail.com>
2024-07-13 17:28:22 +0200

    index.categories - removed ul attributes

--- include.pagebody.index.categories.php
+++ include.pagebody.index.categories.php
@@ -8,7 +8,7 @@ $category_list = $ui->getCategoriesTagCloud($cat_ranked, "non-selected");
 
       <div id="pagebody-categories">
         <a href="/swish-katalogen/k/"><h2>Kategorier</h2></a>
-        <ul role="navigation" aria-label="Webdev tag cloud">
+        <ul>
           <?php echo($category_list); ?>
         </ul>
         Dessa är de största <?php echo($config["content"]["categories"]["index-categories"]); ?> kategorierna, samtliga finns <a href="/swish-katalogen/k/">här</a>
