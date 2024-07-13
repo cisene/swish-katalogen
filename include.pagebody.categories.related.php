560daa4211100e7d37698a4001ac30af03e1bdf1
Christopher Isene <christopher.isene@gmail.com>
2024-07-13 17:30:54 +0200

    categories.related  - removed ul attributes

--- include.pagebody.categories.related.php
+++ include.pagebody.categories.related.php
@@ -12,7 +12,7 @@ $category_list = $ui->getCategoriesTagCloud($cat_related, "non-selected");
 
       <div id="pagebody-categories">
         <a href="/swish-katalogen/k/"><h2>Relaterade Kategorier</h2></a>
-        <ul role="navigation" aria-label="Webdev tag cloud">
+        <ul>
           <?php echo($category_list); ?>
         </ul>
       </div>
