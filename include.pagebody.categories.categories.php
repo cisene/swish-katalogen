8e961f3173ac45c92effd15f562ffe3a407add87
Christopher Isene <christopher.isene@gmail.com>
2024-07-17 18:46:44 +0200

    tweak

--- include.pagebody.categories.categories.php
+++ include.pagebody.categories.categories.php
@@ -9,6 +9,6 @@ $category_list = $ui->getCategoriesTagCloud($cat_ranked, "non-selected");
       <div id="pagebody-categories">
         <a href="/swish-katalogen/k/"><h2>Alla kategorier i alfabetisk ordning</h2></a>
         <ul>
-          <?php echo($category_list); ?>
+<?php echo($category_list); ?>
         </ul>
       </div>
