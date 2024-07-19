951fcfe39480e8c414ceb8e3066e1603c2b94188
Christopher Isene <christopher.isene@gmail.com>
2024-07-20 01:09:30 +0200

    tweak

--- include.pagefooter.php
+++ include.pagefooter.php
@@ -28,3 +28,5 @@
 
       <!-- Begin MediaCreeper tracker code --><a href="https://mediacreeper.com/latest" title="MediaCreeper"><img src="https://mediacreeper.com/image" alt="MediaCreeper" loading="lazy" style="width: 80px;height: 15px;border: 0px; display: none;"></a><!-- End MediaCreeper tracker code -->
       <!-- Begin Creeper tracker code --><a href="https://gnuheter.com/creeper/senaste" title="Creeper"><img src="https://gnuheter.com/creeper/image" alt="Creeper" loading="lazy" style="width: 80px;height: 15px;border: 0px; display: none;"></a><!-- End Creeper tracker code -->
+
+      <a href="/rabbithole/" style="display: none;"></a>
