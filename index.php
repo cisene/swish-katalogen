72c750540731d26e3005ccb13265eb1f62926f98
Christopher Isene <christopher.isene@gmail.com>
2024-07-22 15:42:10 +0200

    tweaks

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ $db->connectDB(
 $siteparam = $config["site"];
 
 $config["content"]["html"]["header"]["title"] = PAGE_START_TITLE;
-$config["content"]["html"]["header"]["description"]["content"] = PAGE_START_DESC;
+$config["content"]["html"]["header"]["meta"]["description"]["content"] = PAGE_START_DESC;
 
 ?><!DOCTYPE html>
 <html lang="sv" dir="ltr" xml:lang="sv" xmlns="http://www.w3.org/1999/xhtml">
