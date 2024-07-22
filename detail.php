d69e11312dd9a00959ea7619bc2bb9c09c87375a
Christopher Isene <christopher.isene@gmail.com>
2024-07-22 16:39:23 +0200

    Detail

--- detail.php
+++ detail.php
@@ -4,7 +4,6 @@ ini_set('display_errors','On');
 
 include_once "site.config.php";
 
-// include_once "class.SqliteDB.php";
 include_once "class.MySQLDB.php";
 include_once "class.SwishFormat.php";
 include_once "class.SwishKatalogen.php";
@@ -20,14 +19,12 @@ $db->connectDB(
   $dbparam["port"],
   $dbparam["username"],
   $dbparam["password"],
-  $dbparam["database"]
+  $dbparam["database"],
 );
 
 $siteparam = $config["site"];
 
 $cat_route = $ui->getCategoryRouting();
-
-// $formatted_title_entry = $sf->getSwishSpecificFormat(strval($cat_route), "common");
 
 if(isset($cat_route)) {
   if($cat_route != null) {
@@ -51,16 +48,46 @@ if(isset($cat_route)) {
     }
   }
 }
+
+$detail_title = preg_replace('/\x25orgName\x25/six', $orgName, strval(PAGE_DETAIL_TITLE));
+$detail_desc = preg_replace('/\x25orgName\x25/six', $orgName, strval(PAGE_DETAIL_DESC));
+
+$detail_title = preg_replace('/\x25entry\x25/six', $cat_route, strval($detail_title));
+$detail_desc = preg_replace('/\x25entry\x25/six', $cat_route, strval($detail_desc));
+
+
+$config["content"]["html"]["header"]["title"] = $detail_title;
+$config["content"]["html"]["header"]["meta"]["description"]["content"] = $detail_desc;
+
+$config["content"]["html"]["header"]["meta"]["og_title"]["content"] = $detail_title;
+$config["content"]["html"]["header"]["meta"]["og_description"]["content"] = $detail_desc;
+
+$config["content"]["html"]["header"]["meta"]["dc_title"]["content"] = $detail_title;
+$config["content"]["html"]["header"]["meta"]["dc_description"]["content"] = $detail_desc;
+
 ?><!DOCTYPE html>
 <html lang="sv" dir="ltr" xml:lang="sv" xmlns="http://www.w3.org/1999/xhtml">
   <head prefix="dc: http://purl.org/dc/elements/1.1/; og: http://ogp.me/ns#">
-    <?php echo($ui->renderHTMLHeadMetas($config["content"]["html"]["header"]["meta"]) . "\n"); ?>
-    <?php echo($ui->renderHTMLHeadLinks($config["content"]["html"]["header"]["link"]) . "\n"); ?>
-    <title><?php echo($cat_route . ' - ' . $orgName); ?> - Swish-Katalogen - Sök och hitta Swish-nummer, en enkel söktjänst för Swish-nummer</title>
+<?php echo($ui->renderHTMLHeadMisc($config)); ?>
+<?php echo($ui->renderHTMLHeadMetas($config["content"]["html"]["header"]["meta"]) . "\n"); ?>
+<?php echo($ui->renderHTMLHeadLinks($config["content"]["html"]["header"]["link"]) . "\n"); ?>
     <link rel="stylesheet" href="/swish-katalogen/css/screen.css?nocache=<?php echo(time()); ?>">
     <script src="js/jquery/jquery-3.6.0.min.js"></script>
   </head>
   <body>
+    <script type="application/ld+json">
+      {
+        "@context": "https://schema.org",
+        "@type": "WebPage",
+        "name": "<?php echo($config["site"]["title"]); ?>",
+        "url": "<?php echo($config["site"]["url"]); ?>",
+        "isBasedOn": "https://github.com/cisene/swish-123",
+        "breadcrumb": "Swish-Katalogen - Sök Swish-nummer",
+        "dateCreated": "<?php echo($config["site"]["dateCreated"]); ?>",
+        "datePublished": "<?php echo($config["site"]["dateCreated"]); ?>",
+        "dateModified": "<?php echo($config["site"]["dateModified"]); ?>"
+      }
+    </script>
 
 <?php include_once "include.pageheader.php"; ?>
 
@@ -71,14 +98,12 @@ if(isset($cat_route)) {
 
 <?php include_once "include.pagebody.detail.organization.details.php"; ?>
 
-<?php // include_once "include.pagebody.detail.swish.number.swisha.php"; ?>
-
 <?php include_once "include.pagebody.detail.swish.number.variants.php"; ?>
 
     </section>
 
     <section id="pagefooter">
-      <?php include_once "include.pagefooter.php"; ?>
+<?php include_once "include.pagefooter.php"; ?>
     </section>
 
 
