838a69928f705695844e2dbc120ef2cf6466b0f3
Christopher Isene <christopher.isene@gmail.com>
2024-07-25 16:23:04 +0200

    formatting

--- include.pagebody.detail.swish.number.variants.php
+++ include.pagebody.detail.swish.number.variants.php
@@ -5,5 +5,9 @@ $variants = $ui->getLIFormattedSwishNumberList($number_variants);
 ?>
       <div id="pagebody-swish-number-variants">
         <h3>Variationer på Swish-nummer</h3>
-        <ul><?php echo($variants); ?></ul>
+        <ul>
+
+<?php echo($variants); ?>
+
+        </ul>
       </div>
