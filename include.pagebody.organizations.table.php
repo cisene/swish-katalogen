bcf419311858918f1874ed5e1233069bef1463b4
Christopher Isene <christopher.isene@gmail.com>
2024-08-18 18:37:20 +0200

    Adding Organization-search

---
+++ include.pagebody.organizations.table.php
@@ -0,0 +1,35 @@
+<?php
+
+if($db && $ui) {
+  $category_items = array();
+  $category_item_listing = "";
+
+  $cat_route = $ui->getCategoryRouting();
+  $cat_ranked = $db->getCategoriesAll();
+
+
+  if($cat_route != null) {
+    $category_list = $ui->getCategoriesTagCloud($cat_ranked, $cat_route);
+
+    $category_items = $db->getEntriesByCategory($cat_route);
+    $category_item_listing = $ui->getEntriesCategoryListing($category_items);
+
+  } else {
+    $category_list = $ui->getCategoriesTagCloud($cat_ranked, "non-selected");
+  }
+
+  if(sizeof($category_items) < 1) {
+    $category_list = $ui->getCategoriesTagCloud($cat_ranked, "non-selected");
+  }
+
+}
+
+?>
+
+      <div id="pagebody-organizations-table">
+        <h2>Swishnummer som hör till organisationsnummer '<?php echo($cat_route); ?>'</h2>
+        <table>
+<?php echo($category_item_listing); ?>
+        </table>
+      </div>
+
