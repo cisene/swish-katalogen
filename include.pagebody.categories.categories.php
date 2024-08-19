3e63e8b04d3b796a9de4d12d66018807989e3522
Christopher Isene <christopher.isene@gmail.com>
2024-08-19 12:37:02 +0200

    code cleanup

--- include.pagebody.categories.categories.php
+++ include.pagebody.categories.categories.php
@@ -1,6 +1,5 @@
 <?php
 
-// $cat_ranked = $db->getCategoriesAll();
 $cat_ranked = $db->getCategoriesAscending();
 $category_list = $ui->getCategoriesTagCloud($cat_ranked, "non-selected");
 
--- include.pagebody.organizations.table.php
+++ include.pagebody.organizations.table.php
@@ -1,24 +1,18 @@
 <?php
 
 if($db && $ui) {
-  // $category_items = array();
-  // $category_item_listing = "";
   $org_items = array();
   $org_items_listing = "";
 
   $org_route = $ui->getOrgNumberRouting();
-  // $cat_ranked = $db->getCategoriesAll();
 
 
   if($org_route != null) {
-    // $category_items = $db->getEntriesByCategory($cat_route);
     $org_items = $db->getEntriesByOrgNumber($org_route);
 
-    echo("<pre>"); var_dump($org_items); echo("</pre>");
+    // echo("<pre>"); var_dump($org_items); echo("</pre>");
 
     $org_items_listing = $ui->getEntriesOrgNumberListing($org_items);
-    // $category_item_listing = $ui->getEntriesCategoryListing($category_items);
-
   }
 }
 
