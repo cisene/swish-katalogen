3009b8144b53e68255714a446a813a626f1bbc7e
Christopher Isene <christopher.isene@gmail.com>
2024-08-29 18:07:32 +0200

    Organization toplist

--- class.MySQLDB.php
+++ class.MySQLDB.php
@@ -107,6 +107,23 @@ class MySQLDB {
     return $result;
   }
 
+  public function getToplistOrgNumber($limit = 100) {
+    $result = array();
+    if ($this->mysql_module_loaded == true) {
+      if ($this->db_connection != null) {
+        $query = "SELECT s.orgNumber, COUNT(*) as cnt FROM b19_se.swish s WHERE s.orgNumber NOT LIKE '%-XXXX' GROUP BY s.orgNumber HAVING cnt >= 2 ORDER BY cnt DESC, s.orgNumber ASC LIMIT " . $limit . ";";
+        $results = $this->db_connection->query($query);
+        while ($row = $results->fetch_assoc()) {
+          $result = array(
+            'orgNumber'   => strval($row['orgNumber']),
+            'count'       => strval($row['cnt'])
+          );
+        }
+      }
+    }
+    return $result;
+  }
+
   public function getEntryByID($entry_id) {
     $result = array();
     if ($this->mysql_module_loaded == true) {
--- include.pagebody.organizations.table.toplist.php
+++ include.pagebody.organizations.table.toplist.php
@@ -8,17 +8,18 @@ if($db && $ui) {
 
 
   if($org_route != null) {
-    $org_items = $db->getEntriesByOrgNumber($org_route);
+    // $org_items = $db->getEntriesByOrgNumber($org_route);
+    $org_items = $db->getToplistOrgNumber(100);
 
-    // echo("<pre>"); var_dump($org_items); echo("</pre>");
+    echo("<pre>"); var_dump($org_items); echo("</pre>");
 
-    $org_items_listing = $ui->getEntriesOrgNumberListing($org_items);
+    // $org_items_listing = $ui->getEntriesOrgNumberListing($org_items);
   }
 }
 
 ?>
 
-      <div id="pagebody-organizations-table">
+      <div id="pagebody-organizations-toplist">
         <h2>Swishnummer som hör till organisationsnummer '<?php echo($org_route); ?>'</h2>
         <table>
 <?php echo($org_items_listing); ?>
