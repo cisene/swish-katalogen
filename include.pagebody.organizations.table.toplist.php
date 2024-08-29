3e1b5dbb81fae7439991324a096a932112968438
Christopher Isene <christopher.isene@gmail.com>
2024-08-29 18:43:41 +0200

    Organization toplist

--- include.pagebody.organizations.table.toplist.php
+++ include.pagebody.organizations.table.toplist.php
@@ -4,13 +4,13 @@ if($db && $ui) {
   $org_items = array();
   $org_items_listing = "";
 
-  $org_route = $ui->getOrgNumberRouting();
+  // $org_route = $ui->getOrgNumberRouting();
 
   $org_items = $db->getToplistOrgNumber(100);
 
   // echo("<pre>"); var_dump($org_items); echo("</pre>");
 
-  $org_items_toplist = $ui->getOrgNumberTopListing($items);
+  $org_items_toplist = $ui->getOrgNumberTopListing($org_items);
 }
 
 ?>
