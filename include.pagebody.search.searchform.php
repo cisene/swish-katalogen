9355d98d3c4e5dd946f360928a47c5190411292e
Christopher Isene <christopher.isene@gmail.com>
2024-09-01 15:32:41 +0200

    Search page cleanup

--- include.pagebody.search.searchform.php
+++ include.pagebody.search.searchform.php
@@ -2,3 +2,11 @@
 
 
 ?>
+
+    <div id="search-form">
+      <form action="#" method="GET">
+        <label for="search">Fritext</label>
+        <input name="search" placeholder="" value="">
+        <button>Sök</button>
+      </form>
+    </div>
