c31ac4f8c4a92d84e062b6a06e3e614b46efb039
Christopher Isene <christopher.isene@gmail.com>
2024-09-22 17:05:41 +0200

    Update

--- include.pagebody.index.intro.php
+++ include.pagebody.index.intro.php
@@ -7,5 +7,5 @@
         <p>Skatteverket godkänner organisationer som godända mottagare av donationer och gåvor, som i vissa fall är skattefria eller avdragbara, läs mer på Skatteverket <a href="https://www.skatteverket.se/privat/skatter/arbeteochinkomst/skattereduktioner/skattereduktionforgavor.4.5fc8c94513259a4ba1d800064144.html" target="_blank">Skattereduktion för gåvor</a>, du finner dem här <a href="/swish-katalogen/k/godk%C3%A4nd+g%C3%A5vomottagare">godkända gåvomottagare</a>.
         <br>
         <h2>Insamlingskontroll</h2>
-        <p>Organisationen Insamlingskontroll utger så kallade 90-konton och kontrollerar organisationer som samlar in donationer, dessa finns att hitta här: <a href="/swish-katalogen/k/insamlingskontroll">Insamlingskontroll</a>.</p>
+        <p>Organisationen <a href="https://www.insamlingskontroll.se/" target="_blank">Insamlingskontroll</a> utger så kallade 90-konton och kontrollerar organisationer som samlar in donationer, dessa finns att hitta här: <a href="/swish-katalogen/k/insamlingskontroll">Insamlingskontroll</a>.</p>
       </div>
