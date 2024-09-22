929f97d94298bde7aec65a620a50aa5fb4e548db
Christopher Isene <christopher.isene@gmail.com>
2024-09-22 17:08:25 +0200

    Updated text

--- include.pagebody.index.intro.php
+++ include.pagebody.index.intro.php
@@ -4,8 +4,8 @@
         <p>Swish-Katalogen är en katalog eller enkel sök-motor, genom att alla samlade Swish-123 nummer är kategoriserade med minst en kategori, kan dessa sökas fram genom att klicka runt i tag-molnet.<br>Katalogen har skapats på grund av bristen av sökbarhet och ren nyfikenhet, "vem har Swish-nummer 123 xxx xx xx?!" eller bara kolla swish nummer.</p>
         <br>
         <h2>Skatteverkets godkända gåvomottagare</h2>
-        <p>Skatteverket godkänner organisationer som godända mottagare av donationer och gåvor, som i vissa fall är skattefria eller avdragbara, läs mer på Skatteverket <a href="https://www.skatteverket.se/privat/skatter/arbeteochinkomst/skattereduktioner/skattereduktionforgavor.4.5fc8c94513259a4ba1d800064144.html" target="_blank">Skattereduktion för gåvor</a>, du finner dem här <a href="/swish-katalogen/k/godk%C3%A4nd+g%C3%A5vomottagare">godkända gåvomottagare</a>.
+        <p>Skatteverket godkänner organisationer som godända mottagare av donationer och gåvor, som i vissa fall är skattefria eller avdragbara, läs mer på Skatteverket <a href="https://www.skatteverket.se/privat/skatter/arbeteochinkomst/skattereduktioner/skattereduktionforgavor.4.5fc8c94513259a4ba1d800064144.html" target="_blank">Skattereduktion för gåvor</a>, Swishnummer märkta med 'godkända gåvomottagare' finner du här <a href="/swish-katalogen/k/godk%C3%A4nd+g%C3%A5vomottagare">godkända gåvomottagare</a>.</p>
         <br>
         <h2>Insamlingskontroll</h2>
-        <p>Organisationen <a href="https://www.insamlingskontroll.se/" target="_blank">Insamlingskontroll</a> utger så kallade 90-konton och kontrollerar organisationer som samlar in donationer, dessa finns att hitta här: <a href="/swish-katalogen/k/insamlingskontroll">Insamlingskontroll</a>.</p>
+        <p>Organisationen <a href="https://www.insamlingskontroll.se/" target="_blank">Svensk Insamlingskontroll</a> utger så kallade 90-konton och kontrollerar organisationer som samlar in donationer, Swishnummer märkta med 'insamlingskontroll' finns att hitta här: <a href="/swish-katalogen/k/insamlingskontroll">Insamlingskontroll</a>.</p>
       </div>
