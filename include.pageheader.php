04a93bfae0c81eb5cd5d2988135812ebb4f34ecb
Christopher Isene <christopher.isene@gmail.com>
2024-12-10 16:21:34 +0100

    Fix PageHeader, Added link to Organisations

--- include.pageheader.php
+++ include.pageheader.php
@@ -1,38 +1,48 @@
+<?php
+$breadcrumbs = array(
+  "@context"  => "https://schema.org",
+  "@type"     => "BreadcrumbList",
+  "itemListElement" => array(
+    array(
+      "@type"     => "ListItem",
+      "position"  => 1,
+      "item"      => array(
+        "@id" => "https://b19.se/swish-katalogen/",
+        "name" => "Hem"
+      )
+    ),
+    array(
+      "@type"     => "ListItem",
+      "position"  => 2,
+      "item"      => array(
+        "@id" => "https://b19.se/swish-katalogen/k/",
+        "name" => "Kategorier"
+      )
+    ),
+    array(
+      "@type"     => "ListItem",
+      "position"  => 3,
+      "item"      => array(
+        "@id" => "https://b19.se/swish-katalogen/s/",
+        "name" => "Sök"
+      )
+    ),
+    array(
+      "@type"     => "ListItem",
+      "position"  => 4,
+      "item"      => array(
+        "@id" => "https://b19.se/swish-katalogen/o/",
+        "name" => "Organisationer"
+      )
+    ),
+
+  )
+);
+?>
     <section id="pageheader">
       <a href="/swish-katalogen/"><h1>Swish-Katalogen</h1></a>
     </section>
 
-    <script type="application/ld+json">
-      {
-        "@context": "https://schema.org",
-        "@type": "BreadcrumbList",
-        "itemListElement": [
-          {
-            "@type": "ListItem",
-            "position": 1,
-            "item": {
-              "@id": "https://b19.se/swish-katalogen/",
-              "name": "Hem"
-            }
-          },
-          {
-            "@type": "ListItem",
-            "position": 2,
-            "item": {
-              "@id": "https://b19.se/swish-katalogen/k/",
-              "name": "Kategorier"
-            }
-          },
-          {
-            "@type": "ListItem",
-            "position": 3,
-            "item": {
-              "@id": "https://b19.se/swish-katalogen/s/",
-              "name": "Sök"
-            }
-          }
-        ]
-      }
-    </script>
+    <script type="application/ld+json"><?php echo(json_encode($breadcrumbs)); ?></script>
 
 
